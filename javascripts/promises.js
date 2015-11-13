056461ee4924b19bce38eb5ddb89639e426d3593
Anderson Cook <andersonmcook@gmail.com>
2015-11-13 13:01:05 -0600

    1pm

--- javascripts/promises.js
+++ javascripts/promises.js
@@ -59,31 +59,6 @@ requirejs(
     });
     console.log("FinalOutside");
 
-    // books.load(function(bookArray) {
-    //   require(['hbs!../templates/books'], function(bookTpl) {
-    //     $("#bookList").html(bookTpl({ books:bookArray }));
-    //   });
-    // });
-
-    /* Here's some pseudo-code for how it should look once you
-       start using promises
-
-    getBookTypes()
-      .then(function(types) {
-        getBooks(types);
-      })
-      .then(function(books) {
-        // add the type key to each book that is currently
-        // being performed in the get-books file
-
-        // then bind the template to the data 
-        // (p.s. make the handlebar template a module dependency)
-        require(['hbs!../templates/books'], function(bookTpl) {
-          $("#bookList").html(bookTpl({ books:bookArray }));
-        });
-
-      })
-     */
 
   }
 );
